2ac652e977667e7dec7c30fe05f11ac4acf486bd
Leigh Ola <eleighola@gmail.com>
2024-03-25 14:14:54 +0100

    edir

--- src/books/dto/books-dto.ts
+++ src/books/dto/books-dto.ts
@@ -43,7 +43,11 @@ export class CreateBookDto {
 
   // cover
   @IsOptional()
-  @IsEnum(BookCoversEnum)
+  @IsEnum(BookCoversEnum, {
+    message:
+      'Cover must be one of the following: ' +
+      Object.values(BookCoversEnum).join(', '),
+  })
   cover?: BookCoversEnum;
 
   // amountInStock
